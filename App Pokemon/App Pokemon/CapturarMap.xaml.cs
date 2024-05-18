2a586aa44fdbb1c574b035cc41779263d8f457e1
Gonzalo De Los Reyes <3bgonzalodelosreyes@gmail.com>
2024-05-18 13:20:45 +0200

    Captura realizada queda enlazar con mis pokemons y algunos ejemplos mas

--- App Pokemon/App Pokemon/CapturarMap.xaml.cs
+++ App Pokemon/App Pokemon/CapturarMap.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Xamarin.Forms.Shapes;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -24,6 +25,8 @@ namespace App_Pokemon
     /// </summary>
     public sealed partial class CapturarMap : Page
     {
+        private Flyout _currentFlyout;
+
         public CapturarMap()
         {
             this.InitializeComponent();
@@ -41,23 +44,108 @@ namespace App_Pokemon
             myMap.ZoomLevel = 6;
 
             // Define las coordenadas de las ciudades
-            AddMapIcon(new BasicGeoposition() { Latitude = 40.416775, Longitude = -3.703790 }, "Madrid");
-            AddMapIcon(new BasicGeoposition() { Latitude = 39.862831, Longitude = -4.027323 }, "Toledo");
-            AddMapIcon(new BasicGeoposition() { Latitude = 38.984783, Longitude = -3.927045 }, "Ciudad Real");
-            AddMapIcon(new BasicGeoposition() { Latitude = 43.361914, Longitude = -5.849389 }, "Asturias");
+            AddMapIcon(new BasicGeoposition() { Latitude = 40.416775, Longitude = -3.703790 }, "Madrid", "Gengar");
+            AddMapIcon(new BasicGeoposition() { Latitude = 39.862831, Longitude = -4.027323 }, "Toledo", "DracoFire");
+            AddMapIcon(new BasicGeoposition() { Latitude = 38.984783, Longitude = -3.927045 }, "Ciudad Real", "Toxicroac");
+            AddMapIcon(new BasicGeoposition() { Latitude = 43.361914, Longitude = -5.849389 }, "Asturias", "Articuno");
+
+            myMap.MapElementClick += MapControl_MapElementClick;
         }
 
-        private void AddMapIcon(BasicGeoposition position, string title)
+        private void AddMapIcon(BasicGeoposition position, string title, string tag)
         {
             var mapIcon = new MapIcon
             {
                 Location = new Geopoint(position),
                 NormalizedAnchorPoint = new Windows.Foundation.Point(0.5, 1.0),
                 Title = title,
+                Tag = tag,
                 ZIndex = 0
             };
 
             myMap.MapElements.Add(mapIcon);
+        }
+
+        private void MapControl_MapElementClick(MapControl sender, MapElementClickEventArgs args)
+        {
+
+            if (args.MapElements.FirstOrDefault() is MapIcon clickedIcon)
+            {
+
+                if (clickedIcon.Title.ToString() == "Madrid")
+                {
+                    Frame.Navigate(typeof(CapturarPage), "Gengar");
+                }
+                else if (clickedIcon.Title.ToString() == "Toledo")
+                {
+                    Frame.Navigate(typeof(CapturarPage), "DracoFire");
+                }
+
+            }
+        }
+
+
+
+
+
+        private void MapControl_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            var point = e.GetCurrentPoint(myMap);
+            var position = point.Position;
+
+            // Convert screen coordinates to geographical coordinates
+            Geopoint geoPoint;
+            myMap.GetLocationFromOffset(position, out geoPoint);
+
+            foreach (var mapElement in myMap.MapElements)
+            {
+                if (mapElement is MapIcon mapIcon)
+                {
+                    var iconPosition = mapIcon.Location.Position;
+
+                    // Check if the cursor is close to the MapIcon (within a small threshold)
+                    if (GetDistance(iconPosition, geoPoint.Position) < 0.01) // threshold in degrees
+                    {
+                        ShowFlyout(mapIcon, position);
+                        return;
+                    }
+                }
+            }
+
+            // If no MapIcon is found under the cursor, close the current Flyout
+            _currentFlyout?.Hide();
+        }
+
+        private double GetDistance(BasicGeoposition pos1, BasicGeoposition pos2)
+        {
+            double latDiff = pos1.Latitude - pos2.Latitude;
+            double lonDiff = pos1.Longitude - pos2.Longitude;
+            return Math.Sqrt(latDiff * latDiff + lonDiff * lonDiff);
+        }
+
+        private void ShowFlyout(MapIcon mapIcon, Windows.Foundation.Point position)
+        {
+            // Close the current Flyout if it exists
+            _currentFlyout?.Hide();
+
+            var flyout = new Flyout();
+            var stackPanel = new StackPanel();
+
+            var titleTextBlock = new TextBlock { Text = mapIcon.Title, FontWeight = Windows.UI.Text.FontWeights.Bold };
+            var subtitleTextBlock = new TextBlock { Text = mapIcon.Tag as string };
+
+            stackPanel.Children.Add(titleTextBlock);
+            stackPanel.Children.Add(subtitleTextBlock);
+
+            flyout.Content = stackPanel;
+            _currentFlyout = flyout;
+
+            // Create FlyoutShowOptions and set its position
+            var showOptions = new FlyoutShowOptions
+            {
+                Position = position
+            };
+            flyout.ShowAt(myMap, showOptions);
         }
     }
 }
---
+++ App Pokemon/App Pokemon/CapturarPage.xaml.cs
@@ -0,0 +1,156 @@
+﻿using ClassLibrary1_Prueba;
+using ControlUsuario_IPO2;
+using LucarioGAC;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Pokemon_Antonio_Campallo_Gomez;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App_Pokemon
+{
+    /// <summary>
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class CapturarPage : Page
+    {
+
+        DispatcherTimer dtTimeB;
+        bool direccionBarra = true;
+        string ipokemon;
+
+        public CapturarPage()
+        {
+            this.InitializeComponent();
+            configurar_pokemons();
+            pbMove();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e != null)
+            {
+                string str = e.Parameter as string;
+                if (str == "Gengar")
+                {
+                    this.ipokemon = "Gengar";
+                    vbGengar.Visibility = Visibility.Collapsed;
+                    vbDracofire.Visibility = Visibility.Visible;
+                }
+                else if (str == "DracoFire")
+                {
+                    this.ipokemon = "DracoFire";
+                    vbDracofire.Visibility = Visibility.Collapsed;
+                    vbGengar.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        private void configurar_pokemons()
+        {
+            DracofireGDLRS.verNombre(false);
+            DracofireGDLRS.verPocionVida(false);
+            DracofireGDLRS.verPocionEnergia(false);
+            DracofireGDLRS.verFilaEnergia(false);
+            DracofireGDLRS.verFilaVida(false);
+
+            //ArticunoACG.verNombre(false);
+            //ArticunoACG.verPocionVida(false);
+            //ArticunoACG.verPocionEnergia(false);
+
+            GengarJCC.verNombre(false);
+            GengarJCC.verPocionVida(false);
+            GengarJCC.verPocionEnergia(false);
+            GengarJCC.verFilaEnergia(false);
+            GengarJCC.verFilaVida(false);
+
+            //MyUCLucario.verNombre(false);
+            //MyUCLucario.verPocionVida(false);
+            //MyUCLucario.verPocionEnergia(false);
+
+            //ToxicroackJPG.verNombre(false);
+            //ToxicroackJPG.verPocionVida(false);
+            //ToxicroackJPG.verPocionEnergia(false);
+
+            //DragoniteCSD.verNombre(false);
+            //DragoniteCSD.verPocionVida(false);
+            //DragoniteCSD.verPocionEnergia(false);
+        }
+
+        private void pbMove()
+        {
+            dtTimeB = new DispatcherTimer();
+            dtTimeB.Interval = TimeSpan.FromMilliseconds(5);
+            dtTimeB.Tick += move;
+            dtTimeB.Start();
+        }
+
+        private void move(object sender, object e)
+        {
+            if (this.direccionBarra == true)
+            {
+                this.pbFuerza.Value += 0.5;
+                if (pbFuerza.Value >= 100)
+                {
+                    this.direccionBarra = false;
+                }
+            }
+            else if (this.direccionBarra == false)
+            {
+                this.pbFuerza.Value -= 0.5;
+                if (pbFuerza.Value <= 0)
+                {
+                    this.direccionBarra = true;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.dtTimeB.Stop();
+
+            if (40 <= this.pbFuerza.Value && this.pbFuerza.Value <= 60)
+            {
+                Storyboard sbaux = (Storyboard)this.Resources["LanzarBien"];
+                sbaux.Begin();
+                sbaux.Completed += (s, ev) => { sbaux.Stop();
+                    ToastContentBuilder notificacion = new ToastContentBuilder();
+                    notificacion.AddArgument("action", "Captura")
+                    .AddText("¡Pokemon Capturado!")
+                    .AddText("Has capturado un " + ipokemon)
+                    .AddAppLogoOverride(new Uri("ms-appx:///Assets/Logo.png"), ToastGenericAppLogoCrop.Circle)
+                    .SetToastDuration(0)
+                    .Show();
+
+                    Frame.Navigate(typeof(MisPokemonPage), ipokemon);
+                };
+            }
+            else { 
+                Storyboard sbaux = (Storyboard)this.Resources["LanzarMal"];
+                sbaux.Begin();
+                sbaux.Completed += (s, ev) => { sbaux.Stop();
+                    ToastContentBuilder notificacion = new ToastContentBuilder();
+                    notificacion.AddArgument("action", "FalloCaptura")
+                    .AddText("¡Captura Pokemon Fallida!")
+                    .AddAppLogoOverride(new Uri("ms-appx:///Assets/imgCaptura/Error.png"), ToastGenericAppLogoCrop.Circle)
+                    .SetToastDuration(0)
+                    .Show();
+                };
+            }
+        }
+    }
+}
